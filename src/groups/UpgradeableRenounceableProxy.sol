be1657c4e3b0b9db0fe1d4fec205c77aa5d6765d
Benjamin Bollen <ben@doru.io>
2024-11-08 16:24:54 +0000

    Update src/groups/UpgradeableRenounceableProxy.sol
    Co-authored-by: Yevgeniy <35062472+roleengineer@users.noreply.github.com>

--- src/groups/UpgradeableRenounceableProxy.sol
+++ src/groups/UpgradeableRenounceableProxy.sol
@@ -10,39 +10,47 @@ interface IUpgradeableRenounceableProxy {
 contract UpgradeableRenounceableProxy is ERC1967Proxy {
     // Errors
 
-    error OnlyAdminError();
-
     error BlockReceive();
+    
+    // Constants
 
-    // Modifier
-
-    modifier onlyAdmin() {
-        if (msg.sender != ERC1967Utils.getAdmin()) {
-            revert OnlyAdminError();
-        }
-        _;
-    }
+    /// @dev Initial proxy admin.
+    address internal immutable ADMIN_INIT;
 
     // Constructor
 
     constructor(address _implementation, bytes memory _data) ERC1967Proxy(_implementation, _data) {
         // set the admin to the deployer
         ERC1967Utils.changeAdmin(msg.sender);
+        // set the admin as immutable
+        ADMIN_INIT = msg.sender;
     }
 
     function implementation() external view returns (address) {
         return _implementation();
     }
 
-    function upgradeToAndCall(address _newImplementation, bytes memory _data) external onlyAdmin {
-        ERC1967Utils.upgradeToAndCall(_newImplementation, _data);
+    /// @dev Dispatch if caller is admin.
+    function _fallback() internal virtual override {
+        if (msg.sender == ADMIN_INIT && msg.sender == ERC1967Utils.getAdmin()) {
+            _dispatchAdmin();
+        } else {
+            super._fallback();
+        }
     }
 
-    /**
-     * @dev It renounces the ability to upgrade the contract, by setting the admin to 0x01.
-     */
-    function renounceUpgradeability() external onlyAdmin {
-        ERC1967Utils.changeAdmin(address(0x01));
+    /// @dev Upgrades to new implementation, renounces the ability to upgrade or moves to regular flow based on admin request.
+    function _dispatchAdmin() private {
+        if (msg.sig == IUpgradeableRenounceableProxy.upgradeToAndCall.selector) {
+            // upgrades to new implementation
+            (address newImplementation, bytes memory data) = abi.decode(msg.data[4:], (address, bytes));
+            ERC1967Utils.upgradeToAndCall(newImplementation, data);
+        } else if (msg.sig == IUpgradeableRenounceableProxy.renounceUpgradeability.selector) {
+            // renounces the ability to upgrade the contract, by setting the admin to 0x01.
+            ERC1967Utils.changeAdmin(address(0x01));
+        } else {
+            _delegate(_implementation());
+        }
     }
 
     // Fallback function
